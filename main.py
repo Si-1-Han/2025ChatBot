5a8151a16a5b52be71472bbf6f0ba7b34f3d371b
한시원 <sw51love@naver.com>
2025-03-24 03:17:24 +0900

    20250324실습

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("py")
+import requests as req
+from bs4 import BeautifulSoup as bs
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+}
+
+res = req.get("http://finance.naver.com/sise/sise_quant.naver", headers=headers)
+soup = bs(res.text, "lxml")
+
+# soup 데이터에서 필요한 요소 검색하기
+title = soup.select("a.tltle")
+price = soup.select("td.number:n")
+
+# 리스트의 데이터 길이를 확인해서 검증(len())
+print(len(price))
+
+#
+#contentarea > div.box_type_l > table > tbody > tr:nth-child(3) > td:nth-child(3)
