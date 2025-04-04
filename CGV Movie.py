2d17d4e81b375d93d17ea6efd6bed61560610988
유재웅 <allen08120812@gmail.com>
2025-04-04 16:54:40 +0900

    Add CGV + KOBIS 영화 정보 수집 스크립트

---
+++ CGV Movie.py
@@ -0,0 +1,67 @@
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import json
+
+
+kobis_api_key = "662d65ebb4eb2d7503390ff6d39f26fd"
+target_date = (datetime.datetime.today() - datetime.timedelta(days=1)).strftime("%Y%m%d")  # 어제 날짜
+
+kobis_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
+params = {
+    "key": kobis_api_key,
+    "targetDt": target_date
+}
+
+res = requests.get(kobis_url, params=params)
+kobis_data = res.json()
+
+movie_list = []
+for movie in kobis_data['boxOfficeResult']['dailyBoxOfficeList']:
+    movie_list.append({
+        "rank": movie['rank'],
+        "title": movie['movieNm'],
+        "open_date": movie['openDt'],
+        "audience": movie['audiAcc'],
+        "ticketing": "N/A"
+    })
+
+
+cgv_url = "http://www.cgv.co.kr/movies/?lt=1&ft=0"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
+}
+
+req = requests.get(cgv_url, headers=headers)
+soup = BeautifulSoup(req.text, "html.parser")
+movies = soup.select(".sect-movie-chart ol li")
+
+cgv_data = {}
+for movie in movies:
+    title_tag = movie.select_one(".title")
+    ticketing_tag = movie.select_one(".percent")
+
+    if title_tag and ticketing_tag:
+        title = title_tag.text.strip()
+        ticketing = ticketing_tag.get_text(strip=True)
+        cgv_data[title] = ticketing
+
+
+for movie in movie_list:
+    for cgv_title in cgv_data:
+        if cgv_title.replace(" ", "") in movie['title'].replace(" ", ""):
+            movie['ticketing'] = cgv_data[cgv_title]
+            break
+
+
+with open("movies_with_ticketing.json", "w", encoding="utf-8") as f:
+    json.dump(movie_list, f, ensure_ascii=False, indent=2)
+
+
+
+for movie in movie_list:
+    print(f"{movie['rank']}위 | {movie['title']}")
+    print(f"  개봉일    : {movie['open_date']}")
+    print(f"  누적관객수: {movie['audience']}")
+    print(f"  CGV 예메율 :{movie['ticketing']}")
+    print("-" * 40)
